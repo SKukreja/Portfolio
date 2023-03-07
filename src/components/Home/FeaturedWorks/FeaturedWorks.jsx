2d91f33f4f29db372b367e161f9ffe075f04dc44
Sumit Kukreja <sumit.kukreja@live.com>
2023-03-06 23:56:54 -0500

    featured works animation

--- src/components/Home/FeaturedWorks/FeaturedWorks.jsx
+++ src/components/Home/FeaturedWorks/FeaturedWorks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components';
+import { InView, useInView } from 'react-intersection-observer';
 import use from '../../../hooks/use';
 
 const hueRotate = keyframes`
@@ -22,13 +23,42 @@ const Featured = styled.div`
   align-items: center;
 `;
 
-const Project = styled.div`
-    display: flex;
+const pulseBackground = keyframes`
+  0% {
+    opacity: 1;
+    left:0;
+    filter: contrast(7) grayscale(0.7);
+  }
+  16.6% {
+    opacity: 0.5;
+    filter: contrast(3) grayscale(0.7);
+  }
+  33.3% {
+    opacity: 1;
+    filter: contrast(7) grayscale(0.7);
+  }
+  50% {
+    opacity: 0.5;
+    filter: contrast(3) grayscale(0.7);
+  }
+  66.7% {
+    opacity: 1;
+    filter: contrast(7) grayscale(0.7);
+  }
+  83.3% { 
+    opacity: 0.5;
+    filter: contrast(3) grayscale(0.7);
+  }
+  100% {
+    opacity: 1;
+    left: -15%;
+    filter: contrast(7) grayscale(0.7);
+  }
 `;
 
 const ProjectBackground = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 120%;
+  height: 120%;
   z-index: 0;
   position: absolute;
   left: 0;
@@ -36,8 +66,9 @@ const ProjectBackground = styled.div`
   top: 0;
 	background: linear-gradient(to bottom left, transparent, #504CCF);
 	filter: contrast(7) grayscale(0.7);
+  animation: ${pulseBackground} 6s linear infinite;
+  animation-play-state: paused;
   --mask: linear-gradient(red, rgba(0, 0, 0, 0.45));
-
 	&::before {
 		position: absolute;
 		top: 0; right: 0; bottom: 0; left: 0;
@@ -46,17 +77,6 @@ const ProjectBackground = styled.div`
     mask: var(--mask);
 		content: ''
 	}
-`;
-
-const ProjectMockups = styled.div`
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  width: 100%;
-  background-size: 175%;
-  background-position: 50%;
-  height: 100%;
 `;
 
 const ProjectName = styled.h1`
@@ -95,20 +115,77 @@ const ProjectImage = styled.div`
   box-shadow: 0 0 3px #504CCf;
   overflow: hidden;
   margin: 1rem;
+  z-index: 1;
   margin-bottom: 5rem;
   display: flex;
   animation: ${hueRotate} 18s linear infinite;
   justify-content: center;
   align-items: center;
-  &:after {
-    width: 100px;
-    height: 100px;
-    position: absolute;
-    content: "";
-    top: 0;
-    right: 0;
-    background: green;
-    z-index: 20;
+`;
+
+const Project = styled.div`
+    display: flex;
+    filter: grayscale(1);
+    transition: filter 2s ease-in-out;
+    &.active {
+      filter: grayscale(0);
+    }
+    &.active .diagonal {
+      animation-play-state: running;
+    }
+    &.active .project-background {
+      animation-play-state: running;
+    } 
+    &.active .vertical {
+      animation-play-state: running;
+    }
+
+`;
+
+const panImageDiagonal = keyframes`
+    0% {
+      background-position: 30% 40%;
+    }
+
+    50% {
+      background-position: 80% 50%;   
+    }
+    
+    100% {
+      background-position: 30% 40%;
+    }
+`;
+
+const panImageVertical = keyframes`
+    0% {
+      background-position: 40% 30%;
+    }
+
+    50% {
+      background-position: 50% 80%;   
+    }
+    
+    100% {
+      background-position: 40% 80%;
+    }
+`;
+
+const ProjectMockups = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  width: 100%;
+  background-size: 175%;
+  background-position: 50%;
+  height: 100%;
+  &.diagonal {
+    animation: ${panImageDiagonal} 50s ease-in-out infinite;
+    animation-play-state: paused;
+  }
+  &.vertical {
+    animation: ${panImageVertical} 50s ease-in-out infinite;
+    animation-play-state: paused;
   }
 `;
 
@@ -142,27 +219,31 @@ const FeaturedWorks = () => {
   return (
     <Featured>
       {data?.attributes.featured.works.data.map((project, number) => (
-        <Project key={project.id}>
-          {number % 2 == 0 ? (
-            <ProjectContent className='odd'>
-                <ProjectNumber>{padNum(number + 1, 2)}</ProjectNumber>
-                <ProjectName>{project.attributes.title}</ProjectName>
-                <ProjectSummary className='summary'>{project.attributes.summary}</ProjectSummary>
-            </ProjectContent>
-            ):''}
-          <ProjectImage>
-            <ProjectBackground />
-            <ProjectMockups style={{backgroundImage: "url(" + import.meta.env.VITE_APP_UPLOAD_URL + project.attributes.cover.data.attributes.url + ")"}} />
-          </ProjectImage>
+        <InView key={project.id}>
+          {({ inView, ref, entry }) => (
+          <Project ref={ref} className={`${inView ? 'active' : ''}`}>
+            {number % 2 == 0 ? (
+              <ProjectContent className='odd'>
+                  <ProjectNumber>{padNum(number + 1, 2)}</ProjectNumber>
+                  <ProjectName>{project.attributes.title}</ProjectName>
+                  <ProjectSummary className='summary'>{project.attributes.summary}</ProjectSummary>
+              </ProjectContent>
+              ):''}
+            <ProjectImage>
+              <ProjectBackground className='project-background' />
+              <ProjectMockups className={`${project.attributes.cover_style}`} style={{backgroundImage: "url(" + import.meta.env.VITE_APP_UPLOAD_URL + project.attributes.cover.data.attributes.url + ")"}} />
+            </ProjectImage>
 
-          {number % 2 != 0 ? (
-            <ProjectContent className='even'>
-                <ProjectNumber>{padNum(number + 1, 2)}</ProjectNumber>
-                <ProjectName>{project.attributes.title}</ProjectName>
-                <ProjectSummary className='summary'>{project.attributes.summary}</ProjectSummary>
-            </ProjectContent>
-            ):''}
-        </Project>
+            {number % 2 != 0 ? (
+              <ProjectContent className='even'>
+                  <ProjectNumber>{padNum(number + 1, 2)}</ProjectNumber>
+                  <ProjectName>{project.attributes.title}</ProjectName>
+                  <ProjectSummary className='summary'>{project.attributes.summary}</ProjectSummary>
+              </ProjectContent>
+              ):''}
+          </Project>
+        )}
+        </InView>
       ))}
     </Featured>
   )
